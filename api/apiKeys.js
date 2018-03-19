64d8a47be6f57cdc9a9f17323bfabb2b3cce1a98
laradashut <lara.dashut@gmail.com>
2018-03-19 11:27:51 -0700

    Delete apiKeys.js

--- api/apiKeys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  consumer_key:         'eUZGqkylX2riPx4Ix0T4l78R8',
-  consumer_secret:      'kU1NWDS67wLguqsuSCn0MRpDUTwoHOK2UVg0fG111ZwHgr9q8r',
-  access_token:         '956232756647772160-9nXhRFEmGOOpjuiDG2fhQQxpjM5nZWN',
-  access_token_secret:  '6f4lg2vsltJDIlBfXhXyxEYgxholzxG9e9p0KqJASE4Ci'
-};
